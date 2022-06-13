24cf77c01f3241e76f5daec6dcc7aafb2501ab88
Ewicks <ethanowicks9@gmail.com>
2022-06-13 13:09:19 +0000

    Add User class

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import colorama
+from colorama import Fore, Style
+
+class User:
+    def __init__(self, fname, lname, age):
+        self.fname = fname
+        self.lname = lname
+        self.age = age
+
+    def user_detials(self):
+        f"Hello {self.fname} {self.lname} welcome to our advanced banking system"
