952b9b4d03e5b5e208ed842c3a1985822ceef93e
Ewicks <ethanowicks9@gmail.com>
2022-06-13 14:28:58 +0000

    Clear read me

--- banking.py
+++ banking.py
@@ -32,7 +32,9 @@ class Bank(User):  # Create a bank and will inherit from the user
         return f"Your balance is now: {round(self.balance, 2)}"
 
     def withdraws(self):
-        wd = float(input(f"{self.fname.title()}, please enter how much you would like to withdraw"))
+        wd = float(input(
+            f"{self.fname.title()}, please enter how much you "
+            f"would like to withdraw"))
         if self.balance < wd:
             return "You can't withdraw that amount"
         else:
@@ -43,27 +45,40 @@ class Bank(User):  # Create a bank and will inherit from the user
 
 # class crypt portfolio:
 
-# - display all crypto exchange rates in real time
+# TODO: - display all crypto exchange rates in real time
 
-# - ask user how much and which crypto they want to invest in
+# TODO: - ask user how much and which crypto they want to invest in
 
-# - functions to exchange amount user wants exchanged to specific crypto
+# TODO: - functions to exchange amount user wants exchanged to specific crypto
 
-# - displays all crypto with amount invested in each crypto currency
+# TODO: - displays all crypto with amount invested in each crypto currency
 
 
 def get_int(self):
-        """
-        Function that handles user input for different numbers. As long as
-        the input value is determined to be an integer, it is returned.
-        Otherwise, a ValueError is raised to print the message below.
-        """
-        while True:
-            try:
-                return int(input(self))
+    """
+    Function that handles user input for different numbers. As long as
+    the input value is determined to be an integer, it is returned.
+    Otherwise, a ValueError is raised to print the message below.
+    """
+    while True:
+        try:
+            return int(input(self))
 
-            except ValueError:
-                print(f"\nNot an integer, try again\n")
+        except ValueError:
+            print(f"\n {self} Not an integer, try again\n")
+
+def get_str(self):
+    """
+    Function that handles user input for different numbers. As long as
+    the input value is determined to be an integer, it is returned.
+    Otherwise, a ValueError is raised to print the message below.
+    """
+    while True:
+        try:
+            return int(input(self))
+
+        except ValueError:
+            print(f"\n {self} Not an integer, try again\n")
 
 
 user_details_confirmation = ''
@@ -76,4 +91,11 @@ while (details):
         fname = input("Enter your first name: ")
         lname = input("Enter your last name: ")
         age = get_int("Enter your age: ")
-        user_details_confirmation = input(f"Type 'yes' to confirm your details\n First name: {fname}\n Last name {lname}\n Age: {age}\n").lower()
+        user_details_confirmation = input(
+            f"Type 'yes' to confirm your details\n "
+            f"First name: {fname}\n "
+            f"Last name {lname}\n "
+            f"Age: {age}\n").lower()
+
+while ()
+
