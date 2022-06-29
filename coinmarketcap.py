fa948637a00c781ecfac39446c6a6373be3c35cc
Ewicks <ethanowicks9@gmail.com>
2022-06-29 10:20:19 +0000

    Get more coins from api

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -11,7 +11,7 @@ url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
 parameters = {
   'start': '1',
-  'limit': '5',
+  'limit': '5000',
   'convert': 'USD'
 }
 headers = {
